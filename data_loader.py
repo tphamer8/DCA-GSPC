7c02f58da4abfdcc1da5cdf14bf1ff16777d61b7
tphamer8 <tm.phamer@gmail.com>
2025-05-12 17:42:10 -0700

    imported yfinance and downloaded s&p 500 daily, weekly, and monthly data

---
+++ data_loader.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+
+
+def downloadData(ticker):
+    data_daily = yf.download(ticker, start="2010-01-01", end="2025-01-01")
+    data_weekly = yf.download(ticker, start="2010-01-01", end="2025-01-01", interval="1wk")
+    data_monthly = yf.download(ticker, start="2010-01-01", end="2025-01-01", interval="1mo")
+    print(data_daily.head())
+    print(data_weekly.head())
+    print(data_monthly.head())
+
+def main():
+    ticker = 'GSPC'
+    downloadData(ticker)
+
+if __name__ == "__main__":
+    main()
